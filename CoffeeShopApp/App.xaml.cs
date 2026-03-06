e8e4d4a6745de3542c28ea17a4ee9d9947546a78
Edson <edson@example.com>
2026-03-06 15:58:28 +0100

    feat: add SplashPage with 2s auto-navigation and rename images to use underscores

--- CoffeeShopApp/App.xaml.cs
+++ CoffeeShopApp/App.xaml.cs
@@ -1,14 +1,14 @@
-﻿namespace CoffeeShopApp;
+namespace CoffeeShopApp;
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
-	}
+    public App()
+    {
+        InitializeComponent();
+    }
 
-	protected override Window CreateWindow(IActivationState? activationState)
-	{
-		return new Window(new AppShell());
-	}
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new Views.SplashPage());
+    }
 }
---
+++ CoffeeShopApp/Views/SplashPage.xaml.cs
@@ -0,0 +1,16 @@
+namespace CoffeeShopApp.Views;
+
+public partial class SplashPage : ContentPage
+{
+    public SplashPage()
+    {
+        InitializeComponent();
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await Task.Delay(2000);
+        Application.Current!.Windows[0].Page = new AppShell();
+    }
+}
